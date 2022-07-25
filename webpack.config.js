330aba1621498fcf35f259b9dfa6974447d01cd9
Maisalbek <maysalbek1972@gmail.com>
2022-07-25 10:01:54 +0600

    final commit

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: ["@babel/polyfill", "./src/index.js"],
+  output: {
+    path: path.resolve(__dirname, "main"),
+    filename: "index.js",
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+  devServer: {
+    port: 3000,
+    static: {
+      directory: path.join(__dirname, "src"),
+    },
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "./src/images"),
+          to: path.resolve(__dirname, "main/images"),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(s[ac]ss)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
